cd1b9789dda68e9c4405b2e6c188aae92acac1da
marklfw <marklfwright7@gmail.com>
2023-11-24 01:14:34 -0500

    Added Full Calendar component

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,10 +1,32 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import resourceTimeGridPlugin from '@fullcalendar/resource-timegrid';
 
 function Hello() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <FullCalendar
+        plugins={[
+          dayGridPlugin,
+          interactionPlugin,
+          timeGridPlugin,
+          resourceTimeGridPlugin,
+        ]}
+        initialView="dayGridMonth"
+        headerToolbar={{
+          left: 'prev,next today',
+          center: 'title',
+          right: 'dayGridMonth,timeGridWeek,timeGridDay',
+        }}
+        editable
+        selectable
+        selectMirror
+        dayMaxEvents
+      />
     </div>
   );
 }
